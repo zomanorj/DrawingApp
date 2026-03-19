4d246ac6d27353975933ead13cbf00fb807d5f35
zomano <zomanoa.rj>
2026-03-19 19:44:31 +0300

    ajout du dessin au doigt avec Canvas et gestion du toucher

--- app/src/main/java/mg/ramat/drawingapp/views/DrawingView.java
+++ app/src/main/java/mg/ramat/drawingapp/views/DrawingView.java
@@ -3,17 +3,55 @@ package mg.ramat.drawingapp.views;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
+import android.view.MotionEvent;
 
 public class DrawingView extends View {
 
+    private  Paint paint;
+    private  float startX, startY, endX, endY;
+
     public DrawingView(Context context) {
         super(context);
+        init();
+    }
+
+    private void init() {
         setBackgroundColor(Color.WHITE);
+
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(8);
+        paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawLine(startX, startY, endX, endY, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                startX = event.getX();
+                startY = event.getY();
+                endX = startX;
+                endY = startY;
+                invalidate();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                endX = event.getX();
+                endY = event.getY();
+                invalidate();
+                break;
+        }
+
+        return true;
     }
 }
