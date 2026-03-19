f76950028fa383bdfbf01abb245a52b3c5b30d81
zomano <zomanoa.rj>
2026-03-19 20:20:20 +0300

    ajout de la gestion des couleurs et de l'épaisseur des traits

--- app/src/main/java/mg/ramat/drawingapp/MainActivity.java
+++ app/src/main/java/mg/ramat/drawingapp/MainActivity.java
@@ -1,17 +1,33 @@
 package mg.ramat.drawingapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import mg.ramat.drawingapp.views.DrawingView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawingView drawingView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        DrawingView drawingView = new DrawingView(this);
-        setContentView(drawingView);
+        // IMPORTANT : utiliser un layout XML maintenant
+        setContentView(R.layout.activity_main);
+
+        // récupérer le DrawingView depuis le XML
+        drawingView = findViewById(R.id.drawingView);
+
+        // récupérer boutons
+        Button btnRed = findViewById(R.id.btnRed);
+        Button btnBlue = findViewById(R.id.btnBlue);
+
+        // actions
+        btnRed.setOnClickListener(v -> drawingView.setColor(Color.RED));
+        btnBlue.setOnClickListener(v -> drawingView.setColor(Color.BLUE));
     }
 }
--- app/src/main/java/mg/ramat/drawingapp/views/DrawingView.java
+++ app/src/main/java/mg/ramat/drawingapp/views/DrawingView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.MotionEvent;
 import java.util.ArrayList;
@@ -20,6 +21,22 @@ public class DrawingView extends View {
     private List<Figure> figures = new ArrayList<>();
     private Figure currentFigure = null;
     private int figureType = 2;
+    private int currentColor = Color.RED;
+    private float currentStrokeWidth = 8f;
+
+    public void setFigureType(int type) {
+        this.figureType = type;
+    }
+
+    public void setColor(int color) {
+        this.currentColor = color;
+        paint.setColor(color);
+    }
+
+    public void setStrokeWidth(float width) {
+        this.currentStrokeWidth = width;
+        paint.setStrokeWidth(width);
+    }
 
 
     public DrawingView(Context context) {
@@ -27,12 +44,17 @@ public class DrawingView extends View {
         init();
     }
 
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
     private void init() {
         setBackgroundColor(Color.WHITE);
 
         paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(8);
+        paint.setColor(currentColor);
+        paint.setStrokeWidth(currentStrokeWidth);
         paint.setStyle(Paint.Style.STROKE);
     }
 
